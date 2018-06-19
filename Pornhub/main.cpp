cff6dcb81e6fa1f944cd8743ee1096a503630695
squid.desire <38214456+SquidDesire@users.noreply.github.com>
2018-06-19 22:00:04 +0200

    Add files via upload

---
+++ Pornhub/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+string Gorkzi, Martyna, Filip, Marta;
+
+int main()
+{
+    cout << "Witaj w grze Pornhub The Game" << endl;
+    cout << "Kim chcesz zagrac?"<< endl;
+    cout << "----------------"<<endl;
+    cout << "Gorzki"<<endl;
+    cout << "Martyna"<<endl;
+    cout << "Filip"<<endl;
+    cout << "Marta"<<endl;
+    cout << "----------------";
+    cin >>
+    return 0;
+}
