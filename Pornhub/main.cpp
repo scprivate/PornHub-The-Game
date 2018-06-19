cdc04a1eb5fe37990b9c0e1aa7af248aa5f889ee
M1hal <40405970+M1hal@users.noreply.github.com>
2018-06-19 22:44:13 +0200

    Moze byc?
    Zmieniłem troche to xd

--- Pornhub/main.cpp
+++ Pornhub/main.cpp
@@ -1,19 +1,63 @@
 #include <iostream>
+#include <cstdlib>
+#include <conio.h>
 
 using namespace std;
 
-string Gorkzi, Martyna, Filip, Marta;
+void menu()
+{
+       cout <<endl<< "Witaj w grze Pornhub The Game" << endl<<endl;
+    cout << "Kim chcesz zagrac?"<< endl;
+    cout << "----------------"<<endl;
+    cout << "1. Gorzki"<<endl;
+    cout << "2. Martyna"<<endl;
+    cout << "3. Filip"<<endl;
+    cout << "4. Marta"<<endl;
+    cout << "----------------";
+}
+void wyborpostaci()
+{
+              int wyborpost;
+              wyborpost = getch();
+              switch(wyborpost)
+              {
+              case'1': //Gorzki
+                     {
+                     system("cls");
+                     cout<<endl<<"Grasz Gorzkim.";
+                      break;
+                     }
+
+
+              case'2'://martyna
+                     {
+                     system("cls");
+                     cout<<endl<<"Grasz Martyna.";
+                     break;
+                     }
+
+
+              case'3'://mendzioch
+                     {
+                     system("cls");
+                     cout<<endl<<"Grasz Mendom.";
+                     break;
+                     }
+
+
+              case'4'://martaœka
+                     {
+                     system("cls");
+                     cout<<endl<<"Grasz Martom.";
+                     break;
+                     }
+       }
+}
 
 int main()
 {
-    cout << "Witaj w grze Pornhub The Game" << endl;
-    cout << "Kim chcesz zagrac?"<< endl;
-    cout << "----------------"<<endl;
-    cout << "Gorzki"<<endl;
-    cout << "Martyna"<<endl;
-    cout << "Filip"<<endl;
-    cout << "Marta"<<endl;
-    cout << "----------------";
-    cin >>
+              menu();//menu
+              wyborpostaci();//nwm czm to tak nazwalem
+
     return 0;
 }
